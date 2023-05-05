d0b5eeca46b861e86e023716a39e01635833d720
markziegler1 <mark893b@stud.kea.dk>
2023-05-05 20:01:21 +0200

    added TAILWIND to indextester

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: ["./index.html", "./html/*.html", "./js/*.js"],
   theme: {
-    sm: '480px',
-    md: '768px',
-    lg: '976px',
-    xl: '1440px',
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    }
+
   },
   theme: {
     extend: {},
