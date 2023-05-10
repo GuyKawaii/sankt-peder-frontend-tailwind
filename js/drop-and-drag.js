7b07b68038106bf0526548493155d3392edb38d0
markziegler1 <mark893b@stud.kea.dk>
2023-05-10 17:23:09 +0200

    added drop and drag to responsive menu and added js

--- js/drop-and-drag.js
+++ js/drop-and-drag.js
@@ -1,33 +1,36 @@
+async function getMenu(menuId) {
+    let response = await fetch('http://localhost:8080/menu/' + menuId);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+        return await response.json();
+    }
+}
+
 window.onload = async function () {
-    const menuData = menuPlaceHolder;
-    const menuItems = document.querySelector('#menu-items');
-    const menuName = document.querySelector('#menu-name');
+    try {
+        const menuData = await getMenu(1);  // Fetch menu data from the server
+        const menuItems = document.querySelector('#menu-items');
+        const menuName = document.querySelector('#menu-name');
 
-    menuData.menuItems.forEach((item, index) => {
-        const menuItem = `
-            <div id="menu-item-${index}" class="menu-item bg-gray-200 flex flex-row justify-between max-w-6xl sm:max-w-md">
-                <div class="order-number mr-4">${index + 1}. </div>
-                <div class="menu-item-main flex-grow">
-                    <p class="menu-item-title font-bold">${item.name}</p>
-                    <p class="menu-item-description">${item.description}</p>
+        // Access the nested menuItems array
+        menuData.menuItems[0].menuItems.forEach((item, index) => {
+            const menuItem = `
+                <div id="menu-item-${index}" class="menu-item bg-gray-200 flex flex-row justify-between max-w-6xl sm:max-w-md">
+                    <div class="order-number mr-4">${index + 1}. </div>
+                    <div class="menu-item-main flex-grow">
+                        <p class="menu-item-title font-bold">${item.name}</p>
+                        <p class="menu-item-description">${item.description}</p>
+                    </div>
+                    <span class="menu-item-price ml-4">${item.price},-</span>
                 </div>
-                <span class="menu-item-price ml-4">${item.price},-</span>
-            </div>
-        `;
+            `;
 
-        menuItems.innerHTML += menuItem;
-    });
+            menuItems.innerHTML += menuItem;
+        });
 
-    menuName.innerHTML = menuData.name;
-
-    new Sortable(menuItems, {
-        animation: 150,
-        handle: ".menu-item",
-        onEnd: function() {
-            Array.from(menuItems.children).forEach((child, index) => {
-                const orderNumber = child.querySelector('.order-number');
-                orderNumber.textContent = `${index + 1}. `;
-            });
-        }
-    });
+        menuName.innerHTML = menuData.name;
+    } catch (error) {
+        console.log(error);
+    }
 }
--- js/testMenu.js
+++ js/testMenu.js
@@ -1,37 +1,44 @@
 const menuPlaceHolder = {
     "id": 1,
-    "name": "MENU 11.30 - 21.00",
+    "name": "Updated Menu Name 2",
     "menuItems": [
         {
-            "menu": {
+            "id": 1,
+            "name": "Updated MenuItem 1 2",
+            "description": "Updated description for MenuItem 1",
+            "price": "99,57",
+            "image": {
+                "hibernateLazyInitializer": {},
                 "id": 1,
-                "name": "Updated Menu Name"
-            },
-            "menuItems": [
-
-                {
-                    "id": 2,
-                    "name": "Updated MenuItem 2",
-                    "description": "Updated description for MenuItem 2",
-                    "price": "15.99"
-                },
-                {
-                    "id": 4,
-                    "name": "NEW ITEM",
-                    "description": "NEW ITEM NICE",
-                    "price": "15.99"
-                },
-                {
-                    "id": 1,
-                    "name": "Updated MenuItem 1",
-                    "description": "Updated description for MenuItem 1",
-                    "price": "99.50"
-                },
-            ]
+                "url": "https://example.com/images/karrysild.jpg"
+            }
+        },
+        {
+            "id": 2,
+            "name": "Updated MenuItem 2 2",
+            "description": "Updated description for MenuItem 2",
+            "price": "15,99",
+            "image": {
+                "hibernateLazyInitializer": {},
+                "id": 2,
+                "url": "https://example.com/images/smoked-mackerel.jpg"
+            }
+        },
+        {
+            "id": 3,
+            "name": "NEW ITEM",
+            "description": "NEW ITEM NICE 2",
+            "price": "15,99",
+            "image": {
+                "hibernateLazyInitializer": {},
+                "id": 3,
+                "url": "https://example.com/images/smoked-mackerel.jpg"
+            }
         }
     ]
 }
 
+// Mocking the getMenu function to return placeholder data
 async function getMenu(menuId) {
     return fetch('http://localhost:8080/menu/' + menuId)
         .then(response => response.json())
@@ -43,52 +50,49 @@ async function getMenu(menuId) {
 }
 
 window.onload = async function () {
-    let menuData = await getMenu(1);
-    const menuItems = document.querySelector('#menu-items');
-    const menuName = document.querySelector('#menu-name');
+    try {
+        let menuData = await getMenu(1);
+        const menuItems = document.querySelector('#menu-items');
+        const menuName = document.querySelector('#menu-name');
 
-    menuData.menuItems.forEach((item, index) => {
-        const menuItem = `
-            <div id="menu-item-${item.id}" class="menu-item bg-gray-200 flex flex-row justify-between max-w-6xl sm:max-w-md">
-                <div class="order-number mr-4">${index + 1}. </div>
-                <div class="menu-item-main flex-grow">
-                    <p class="menu-item-title font-bold">${item.name}</p>
-                    <p class="menu-item-description">${item.description}</p>
+        menuData.menuItems.forEach((item, index) => {
+            const menuItem = `
+                <div id="menu-item-${item.id}" class="menu-item bg-gray-200 flex flex-row justify-between max-w-6xl sm:max-w-md">
+                    <div class="order-number mr-4">${index + 1}. </div>
+                    <div class="menu-item-main flex-grow">
+                        <p class="menu-item-title font-bold">${item.name}</p>
+                        <p class="menu-item-description">${item.description}</p>
+                    </div>
+                    <span class="menu-item-price ml-4">${item.price},-</span>
                 </div>
-                <span class="menu-item-price ml-4">${item.price},-</span>
-            </div>
-        `;
+            `;
 
-        menuItems.innerHTML += menuItem;
-    });
+            menuItems.innerHTML += menuItem;
+        });
 
-    menuName.innerHTML = menuData.name;
+        menuName.innerHTML = menuData.name;
 
-    new Sortable(menuItems, {
-        animation: 150,
-        handle: ".menu-item",
-        onEnd: function() {
-            const newOrder = Array.from(menuItems.children).map((child, index) => {
-                const orderNumber = child.querySelector('.order-number');
-                orderNumber.textContent = `${index + 1}. `;
-                const id = parseInt(child.id.replace('menu-item-', ''));
-                return menuData.menuItems.find(item => item.id === id);
-            });
-            menuData.menuItems = newOrder;
-            updateMenu(menuData);
-        }
-    });
+        new Sortable(menuItems, {
+            animation: 150,
+            handle: ".menu-item",
+            onEnd: function() {
+                const newOrder = [];
+                Array.from(menuItems.children).forEach((child, index) => {
+                    const orderNumber = child.querySelector('.order-number');
+                    orderNumber.textContent = `${index + 1}. `;
+                    const id = parseInt(child.id.replace('menu-item-', ''));
+                    newOrder.push(menuData.menuItems.find(item => item.id === id));
+                });
+                menuData.menuItems = newOrder;
+                updateMenu(menuData);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function updateMenu(menuData) {
-    return fetch('http://localhost:8080/menu/' + menuData.id, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(menuData)
-    })
-        .then(response => response.json())
-        .then(data => console.log('Success:', data))
-        .catch((error) => console.error('Error:', error));
+// Mocking the updateMenu function to just log the updated data
+    console.log("Updating menu data:", menuData);
 }
