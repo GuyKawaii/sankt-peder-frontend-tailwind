90a6d780de4e65005faf8f577e61f0264f2d1e48
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-05-17 21:01:17 +0200

    created navbar and used jquery to get css and js specific to navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,21 @@ module.exports = {
     "./js/**/*.js",
   ],
   theme: {
-    screens:{
+    screens: {
       sm: '480px',
       md: '768px',
       lg: '976px',
-      xl: '1440px',
-    }
-
-  },
-  theme: {
+      logo: '1050px',
+    },
     extend: {
       colors: {
         'primary': '#54714d',
         'secondary': {
-            100: '#cf2b2b',
-            200: '#408c42',
-        }
-      }
+          100: '#cf2b2b',
+          200: '#408c42',
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
